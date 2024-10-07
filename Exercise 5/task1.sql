43d6843f0034f822aee2b9741ba8a193c8cfab24
Nilongg <99095762+Nilongg@users.noreply.github.com>
2024-10-07 13:01:03 +0300

    Exercise 5

--- Exercise 5/task1.sql
+++ Exercise 5/task1.sql
@@ -1,3 +1,47 @@
 -- Create a database based on last week's task 3. In that task we designed a database for a blog. The database has
 -- to follow the plan created last week. If you need to update the plan, include an updated drawing of the
 -- database to your return. Add foreign key restrictions for the tables as well.
+
+CREATE DATABASE blog;
+
+USE blog;
+
+-- Table for User
+CREATE TABLE User (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Table for Author (User to Author relationship is 1:1)
+CREATE TABLE Author (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES User(id)
+);
+
+-- Table for Blog (One Author can create many blogs)
+CREATE TABLE Blog (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    author_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    update_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (author_id) REFERENCES Author(id)
+);
+
+-- Table for Comment (Comments belong to a blog, not directly to a user)
+CREATE TABLE Comment (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    blog_id INT NOT NULL,
+    user_id INT NOT NULL,
+    comment_text TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (blog_id) REFERENCES Blog(id)
+    FOREIGN KEY (user_id) REFERENCES User(id)
+);
+--Tehnyt ryhmässä (Niilo V, Mikael N, Aleksi O)
+
+
+
+
--- Exercise 5/task2.sql
+++ Exercise 5/task2.sql
@@ -1,3 +1,21 @@
 -- Add users and roles for the Blog-database. Create roles "author_role" and "commenter_role" and grant those
 -- roles appropriate privileges. Add users "author_app" and "commenter_app". The imaginary webapp uses them for
 -- adding posts and comments to the blog database. Grant appropriate roles for those users.
+
+-- Create roles
+CREATE ROLE author_role;
+CREATE ROLE commenter_role;
+
+-- Grant privileges to roles
+GRANT SELECT, INSERT, UPDATE, DELETE ON blog.Blog TO author_role;
+GRANT SELECT, INSERT ON blog.Comment, blog.Blog TO commenter_role;
+
+-- Create users
+CREATE USER author_app;
+CREATE USER commenter_app;
+
+-- Grant roles to users
+GRANT author_role TO author_app;
+GRANT commenter_role TO commenter_app;
+
+--Tehnyt ryhmässä (Niilo V, Mikael N, Aleksi O)
